995c374ad415d72bcb8d352d740e5d96f1caf228
Tô Anh Tuấn <toanhtuan.18dz@gmail.com>
2026-04-09 19:37:26 +0700

    ss5

---
+++ Session05/src/main/java/com/restaurant/bai5/controller/OrderController.java
@@ -0,0 +1,21 @@
+package com.restaurant.bai5.controller;
+
+import com.restaurant.bai5.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OrderController {
+    @Autowired
+    private OrderService service;
+
+    @GetMapping("/bai5/order")
+    public String orderPage(Model model) {
+        var dishes= service.getMenu();
+        model.addAttribute("dishes", dishes);
+        model.addAttribute("total", service.calculateTotal(dishes));
+        return "order-page";
+    }
+}
