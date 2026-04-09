be7896cd330196493cc338350dbc59920cd9e3bd
Tô Anh Tuấn <toanhtuan.18dz@gmail.com>
2026-04-09 19:43:08 +0700

    ss5

--- Session05/src/main/java/com/restaurant/bai5/controller/OrderController.java
+++ Session05/src/main/java/com/restaurant/bai5/controller/OrderController.java
@@ -4,18 +4,31 @@ import com.restaurant.bai5.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class OrderController {
+
     @Autowired
     private OrderService service;
 
     @GetMapping("/bai5/order")
     public String orderPage(Model model) {
-        var dishes= service.getMenu();
-        model.addAttribute("dishes", dishes);
-        model.addAttribute("total", service.calculateTotal(dishes));
-        return "order-page";
+        try {
+            var dishes = service.getMenu();
+
+            if (dishes == null || dishes.isEmpty()) {
+                throw new RuntimeException("Không có món ăn!");
+            }
+
+            model.addAttribute("dishes", dishes);
+            model.addAttribute("total", service.calculateTotal(dishes));
+
+            return "order-page";
+
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
+            return "error-page";
+        }
     }
 }
